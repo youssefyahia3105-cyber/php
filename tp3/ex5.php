8729344232c922d791f8d09fef6ca3406b0370c3
Youssef yahia <youssef.yahia.31.05@gmail.com>
2026-03-30 00:10:43 +0100

    New feature

--- tp3/ex5.php
+++ tp3/ex5.php
@@ -7,7 +7,32 @@
 </head>
 <body>
     <?php
-        
+        $voyageur=[["prenom"=>"julien" ,"nom"=>"martin" ,"ville"=>"paris"],
+        ["prenom"=>"marc" ,"nom"=>"durand" ,"ville"=>"lille"],
+        ["prenom"=>"marie" ,"nom"=>"dubois" ,"ville"=>"marseille"],
+        ["prenom"=> "julie","nom"=>"dupont" ,"ville"=>"grenoble"]];
+
+        function sauvegarderCsv(array $voyageur):void{
+            $fichier=fopen('voyageur.csv','w');
+            foreach($voyageur as $v){
+                fputcsv($fichier,$v["prenom"],$v["nom"],$v["ville"]);
+            }
+            fclose($fichier);
+        }
+
+        sauvegarderCsv($voyageur);
+
+        $fichier=fopen('voyageur.csv','r');
+        echo "<table border='1'>";
+        while(($ligne=fgetcsv($fichier))!=false){
+            echo "<tr>";
+            foreach($ligne as $cellule){
+                echo"<td>".$cellule."</td>";
+            }
+            echo"</tr>";
+        }
+        echo"</table>";
+        fclose($fichier);
     ?>
 </body>
 </html>
